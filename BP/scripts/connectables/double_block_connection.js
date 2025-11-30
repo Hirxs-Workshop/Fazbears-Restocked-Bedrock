edd23c95c18f63ec8e19a08b79f6b8237cd97b17
ZachDoesGames <156552303+ZachDoesGames@users.noreply.github.com>
2025-11-30 13:09:39 -0600

    changed top to upper in shelf supply closet

--- BP/scripts/connectables/double_block_connection.js
+++ BP/scripts/connectables/double_block_connection.js
@@ -15,7 +15,7 @@ import { system, world, EquipmentSlot, BlockPermutation } from "@minecraft/serve
 system.beforeEvents.startup.subscribe(({ blockComponentRegistry }) => {
     blockComponentRegistry.registerCustomComponent("fr:double_block", {
         beforeOnPlayerPlace(event) {
-            const blockBit = event.permutationToPlace.getState("fr:block_bit");
+            const blockBit = event.permutationupperlace.getState("fr:block_bit");
             
             if (!blockBit || blockBit === 'bottom') {
                 const aboveBlock = event.block.above();
@@ -33,7 +33,7 @@ system.beforeEvents.startup.subscribe(({ blockComponentRegistry }) => {
             if (blockBit === 'bottom') {
                 const aboveBlock = block.above();
                 const direction = block.permutation.getState("minecraft:cardinal_direction");
-                let upperPermutation = block.permutation.withState("fr:block_bit", "top");
+                let upperPermutation = block.permutation.withState("fr:block_bit", "upper");
                 
                 if (direction) {
                     upperPermutation = upperPermutation.withState("minecraft:cardinal_direction", direction);
@@ -43,10 +43,10 @@ system.beforeEvents.startup.subscribe(({ blockComponentRegistry }) => {
             }
         },
         onPlayerBreak({ block, brokenBlockPermutation }) {
-            if (brokenBlockPermutation.getState("fr:block_bit") == 'bottom' && block.above().permutation.getState("fr:block_bit") == 'top') {
+            if (brokenBlockPermutation.getState("fr:block_bit") == 'bottom' && block.above().permutation.getState("fr:block_bit") == 'upper') {
                 block.above().setType("minecraft:air")
             }// ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ ㅤ 
-            if (brokenBlockPermutation.getState("fr:block_bit") == 'top' && block.below().permutation.getState("fr:block_bit") == 'bottom') {
+            if (brokenBlockPermutation.getState("fr:block_bit") == 'upper' && block.below().permutation.getState("fr:block_bit") == 'bottom') {
                 block.below().setType("minecraft:air")
             }
         }
